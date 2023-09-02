9cc9b435ae05cb68eecdc5ba4ef88af819432979
kipelovets <kipelovets@Xeniyas-MacBook-Air.local>
2023-09-02 15:07:46 +0200

    ✨ Tabular and minor improvements

--- nvim/lua/my/init.lua
+++ nvim/lua/my/init.lua
@@ -1,5 +1,9 @@
 require("my.lazy")
-require("lazy").setup("my.plugins")
+require("lazy").setup("my.plugins", {
+    change_detection = {
+        notify = false,
+    },
+})
 
 require("my.remap")
 require("my.settings")
