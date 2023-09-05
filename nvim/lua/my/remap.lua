8b7b1f2acc4a1710ab0be06c734f696c123675d6
Georgii Korshunov <kipelovets@mail.ru>
2023-09-05 22:09:09 +0200

    ✨ toggle-term

--- nvim/lua/my/remap.lua
+++ nvim/lua/my/remap.lua
@@ -66,9 +66,23 @@ nmap('<leader>T', ':NvimTreeToggle<cr>')
 nmap('<leader>s', ':Alpha<cr>')
 
 vim.keymap.set(
-    {"n", "x"},
+    { "n", "x" },
     "<leader>rr",
     function() require('refactoring').select_refactor() end
 )
 
-vim.keymap.set({"i"}, "<Tab>", '<Plug>luasnip-expand-or-jump')
+vim.keymap.set({ "i" }, "<Tab>", '<Plug>luasnip-expand-or-jump')
+
+nmap('<c-`>', ':ToggleTerm<cr>')
+function _G.my_set_terminal_keymaps()
+    local opts = { buffer = 0 }
+    vim.keymap.set('t', '<c-`>', '<cmd>ToggleTerm<cr>', opts)
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua my_set_terminal_keymaps()')
