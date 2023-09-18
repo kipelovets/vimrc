82a0ea360e68ee6e09f6db4f08f66b3220ea37a7
Georgii Korshunov <georgii.korshunov@cdprojektred.com>
2023-09-18 15:25:24 +0200

    🐛 Fix for even longer filenames

--- nvim/lua/my/utils.lua
+++ nvim/lua/my/utils.lua
@@ -10,10 +10,18 @@ M.cd = function(path)
 end
 
 M.shorten_path = function(path)
+    local target_length = 35
     path = path or ""
     path = path:gsub("\\", "/"):gsub("//", "/")
     local pattern = "[^/]+/[^/]+$"
-    return string.sub(path, string.find(path, pattern) or 1)
+    local result = string.sub(path, string.find(path, pattern) or 1)
+    if #result > target_length then
+        result = result:gsub("[^/]+/", "")
+        if #result > target_length then
+            result = result:sub(0, target_length - 3) .. "..."
+        end
+    end
+    return result
 end
 
 return M
