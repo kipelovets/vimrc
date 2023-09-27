bf1358c4c92e7a0d44dcf29688c36ab862cce5be
Georgii Korshunov <kipelovets@mail.ru>
2023-09-27 20:54:22 +0200

    ♻️  Can't find a reliable way to run jobs in parallel, reverting to vim.fn.system

--- nvim/lua/my/dap.lua
+++ nvim/lua/my/dap.lua
@@ -1,30 +1,26 @@
-local Job = require("plenary.job")
+local path = require("plenary.path")
 
-local vscode_php_debug_path = vim.fn.stdpath("data") .. "/vscode-php-debug"
+local vscode_php_debug_path = vim.fn.stdpath("data") .. path.path.sep .. "vscode-php-debug"
 local M = { vscode_php_debug_path = vscode_php_debug_path }
 
 if vim.loop.fs_stat(vscode_php_debug_path) then
     return M
 end
 
-print("vscode-php-debug: cloning into " .. vscode_php_debug_path)
-Job:new({
-    command = "git",
-    args = {
-        "clone",
-        "https://github.com/xdebug/vscode-php-debug.git",
-        vscode_php_debug_path,
-    },
-    on_exit = function(j, return_val)
-        print(return_val)
-        print(vim.json.encode(j:result()))
-    end,
-}):start()
+vim.defer_fn(function()
+    print("vscode-php-debug: cloning")
+    vim.fn.system("git clone https://github.com/xdebug/vscode-php-debug.git " .. vscode_php_debug_path)
 
--- print("vscode-php-debug: installing dependencies ")
--- vim.fn.system("cd " .. vscode_php_debug_path .. " && npm install")
--- print("vscode-php-debug: building")
--- vim.fn.system("cd " .. vscode_php_debug_path .. " && npm run build")
+    print("vscode-php-debug: installing dependencies ")
+    vim.cmd("redraw!")
+    vim.fn.system("cd " .. vscode_php_debug_path .. " && npm install")
+
+    print("vscode-php-debug: building")
+    vim.cmd("redraw!")
+    vim.fn.system("cd " .. vscode_php_debug_path .. " && npm run build")
+
+    print("vscode-php-debug: done!")
+end, 100)
 
 
 return M
