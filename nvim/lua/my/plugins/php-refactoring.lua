270b947eb2bf515ae1f78fef977ae1c0fac8cc2b
Georgii Korshunov <georgii.korshunov@cdprojektred.com>
2023-11-06 13:46:25 +0100

    ✨ New plugins: PHP refactoring & Twig support

---
+++ nvim/lua/my/plugins/php-refactoring.lua
@@ -0,0 +1,8 @@
+return {
+    "adoy/vim-php-refactoring-toolbox",
+    config = function ()
+        vim.g.vim_php_refactoring_use_default_mapping = 0
+        vim.g.vim_php_refactoring_auto_validate_sg = 1
+        vim.g.vim_php_refactoring_auto_validate_g = 1
+    end
+}
---
+++ nvim/lua/my/plugins/twig.lua
@@ -0,0 +1,5 @@
+return {
+    "nelsyeung/twig.vim",
+    lazy = true,
+    event = "BufEnter *.twig"
+}
