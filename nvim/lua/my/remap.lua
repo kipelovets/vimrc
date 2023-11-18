8e58fcc6208bec6be075c0fb96824770d3274555
Georgii Korshunov <kipelovets@mail.ru>
2023-11-19 00:22:06 +0100

    ✨ Added neorepl

--- nvim/lua/my/remap.lua
+++ nvim/lua/my/remap.lua
@@ -184,3 +184,19 @@ nmap('<leader>J',
 
 nmap("[<cr>", ":Hi {<cr>")
 nmap("]<cr>", ":Hi }<cr>")
+
+vim.keymap.set('n', 'g:', function()
+    -- get current buffer and window
+    local buf = vim.api.nvim_get_current_buf()
+    local win = vim.api.nvim_get_current_win()
+    -- create a new split for the repl
+    vim.cmd('split')
+    -- spawn repl and set the context to our buffer
+    require('neorepl').new {
+        lang = 'vim',
+        buffer = buf,
+        window = win,
+    }
+    -- resize repl window and make it fixed height
+    vim.cmd('resize 10 | setl winfixheight')
+end)
