c96bfd1e2de5c81d668e20073b7edbe14eaa9e2d
Georgii Korshunov <georgii.korshunov@chess.com>
2024-04-06 01:12:43 +0200

    🚧 Noice routing: looking for a way to route fugitive output to popup

--- nvim/lua/my/plugins/noice.lua
+++ nvim/lua/my/plugins/noice.lua
@@ -10,5 +10,19 @@ return {
     dependencies = {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
+    },
+    routes = {
+        {
+            view = "confirm",
+            filter = {
+                any = {
+                    {
+                        event = "msg_show",
+                        kind = { "echo", "echomsg", "" },
+                        -- before = true
+                    },
+                },
+            },
+        },
     }
 }
