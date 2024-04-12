dc8be820e01d23afba8bdb6d36b9d908b69f8511
Georgii Korshunov <georgii.korshunov@chess.com>
2024-04-12 23:47:54 +0200

    ♻️ Remap comments + telescope marks

--- nvim/lua/my/remap.lua
+++ nvim/lua/my/remap.lua
@@ -2,7 +2,7 @@ local nmap = function(shortcut, command, desc)
     vim.keymap.set("n", shortcut, command, { desc = desc })
 end
 
-local keybindings = require('my.keybindings')
+local keybindings = require("my.keybindings")
 
 if vim.g.neovide then
     nmap(keybindings.neovide, ":silent exec '!neovide'<cr>")
@@ -20,14 +20,14 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, keybindings.copy, [["+y]])
 vim.keymap.set({ "n", "i" }, keybindings.paste, [[<Esc>"+p]])
 nmap(keybindings.copy_line, '^v$h"+yj')
-nmap(keybindings.duplicate, 'yyp')
-nmap(keybindings.select_all, 'ggVG')
+nmap(keybindings.duplicate, "yyp")
+nmap(keybindings.select_all, "ggVG")
 -- delete into black hole
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-nmap('<leader>/', function()
+nmap("<leader>/", function()
     vim.api.nvim_command(':let @/=""')
-    require('noice').cmd("dismiss")
+    require("noice").cmd("dismiss")
 
     local wins = vim.api.nvim_list_wins()
     for _, win in ipairs(wins) do
@@ -35,113 +35,102 @@ nmap('<leader>/', function()
             vim.api.nvim_win_close(win, false)
         end
     end
-end)
+end, "Clear search & popups")
 
 -- Git
 
-nmap("<leader>gw", ":Gw<cr>")
-nmap("<leader>gs", ":G<cr>")
-nmap("<leader>gc", ":G commit<cr>")
-nmap("<leader>go", ":Git pull<cr>")
-nmap("<leader>gp", ":Git push<cr>")
-nmap("<leader>gf", ":Flog<cr>")
-nmap("<leader>gF", ":Flog -all<cr>")
-nmap("[g", ":lua require'gitsigns'.prev_hunk()<cr>")
-nmap("]g", ":lua require'gitsigns'.next_hunk()<cr>")
+nmap("<leader>gw", ":Gw<cr>", "Git write")
+nmap("<leader>gs", ":G<cr>", "Git status")
+nmap("<leader>gc", ":G commit<cr>", "Git commit")
+nmap("<leader>go", ":Git pull<cr>", "Git pull")
+nmap("<leader>gp", ":Git push<cr>", "Git push")
+nmap("<leader>gf", ":Flog<cr>", "Git log")
+nmap("<leader>gF", ":Flog -all<cr>", "Git log all")
+nmap("[g", ":lua require'gitsigns'.prev_hunk()<cr>", "Goto previous hunk")
+nmap("]g", ":lua require'gitsigns'.next_hunk()<cr>", "Goto next hunk")
 
 -- Buffers
 
-vim.keymap.set({ "n", "i", "v" }, keybindings.save, "<esc>:w<cr>")
+vim.keymap.set({ "n", "i", "v" }, keybindings.save, "<esc>:w<cr>", { desc = "Save" })
 
-nmap(keybindings.prev_tab, ':BufferLineCyclePrev<cr>')
-nmap(keybindings.next_tab, ':BufferLineCycleNext<cr>')
-nmap(keybindings.tab_move_right, ':BufferLineMoveNext<cr>')
-nmap(keybindings.tab_move_left, ':BufferLineMovePrev<cr>')
-nmap('<leader>1', '<Cmd>BufferLineGoToBuffer 1<CR>')
-nmap('<leader>2', '<Cmd>BufferLineGoToBuffer 2<CR>')
-nmap('<leader>3', '<Cmd>BufferLineGoToBuffer 3<CR>')
-nmap('<leader>4', '<Cmd>BufferLineGoToBuffer 4<CR>')
-nmap('<leader>5', '<Cmd>BufferLineGoToBuffer 5<CR>')
-nmap('<leader>6', '<Cmd>BufferLineGoToBuffer 6<CR>')
-nmap('<leader>7', '<Cmd>BufferLineGoToBuffer 7<CR>')
-nmap('<leader>8', '<Cmd>BufferLineGoToBuffer 8<CR>')
-nmap('<leader>9', '<Cmd>BufferLineGoToBuffer 9<CR>')
+nmap(keybindings.prev_tab, ":BufferLineCyclePrev<cr>", "Tab: previous")
+nmap(keybindings.next_tab, ":BufferLineCycleNext<cr>", "Tab: next")
+nmap(keybindings.tab_move_right, ":BufferLineMoveNext<cr>", "Tab: move right")
+nmap(keybindings.tab_move_left, ":BufferLineMovePrev<cr>", "Tab: move left")
+nmap("<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>", "Tab: select #1")
+nmap("<leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>", "Tab: select #2")
+nmap("<leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>", "Tab: select #3")
+nmap("<leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>", "Tab: select #4")
+nmap("<leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>", "Tab: select #5")
+nmap("<leader>6", "<Cmd>BufferLineGoToBuffer 6<CR>", "Tab: select #6")
+nmap("<leader>7", "<Cmd>BufferLineGoToBuffer 7<CR>", "Tab: select #7")
+nmap("<leader>8", "<Cmd>BufferLineGoToBuffer 8<CR>", "Tab: select #8")
+nmap("<leader>9", "<Cmd>BufferLineGoToBuffer 9<CR>", "Tab: select #9")
 
-nmap(keybindings.new_tab, ':enew<cr>')
-nmap('<c-q>', '<Plug>(smartq_this)')
+nmap(keybindings.new_tab, ":enew<cr>", "Tab: new")
+nmap("<c-q>", "<Plug>(smartq_this)", "Tab: close")
 
 -- Telescope
 
-nmap('<leader>fp', ":Telescope project theme=dropdown<cr>")
+nmap("<leader>fp", ":Telescope project theme=dropdown<cr>", "Telescope: projects")
 
-local builtin = require('telescope.builtin')
-nmap(keybindings.find_files, builtin.find_files)
-nmap('<leader>fg', builtin.live_grep)
-nmap('<leader>fG', function()
+local builtin = require("telescope.builtin")
+nmap(keybindings.find_files, builtin.find_files, "Telescope: files")
+nmap("<leader>fg", builtin.live_grep, "Telescope: grep")
+nmap("<leader>fG", function()
     local dir = vim.fn.input("Directory to grep: ", "./", "dir")
     local file_glob = vim.fn.input("File glob pattern: ", "*")
     builtin.live_grep({ search_dirs = { dir }, glob_pattern = file_glob })
-end)
+end, "Telescope: grep in specific dir / glob pattern")
 
-nmap('<leader>fb', builtin.buffers)
-nmap('<leader>fh', builtin.help_tags)
-nmap('<leader>fo', function()
+nmap("<leader>fb", builtin.buffers, "Telescope: open buffers")
+nmap("<leader>fh", builtin.help_tags, "Telescope: help tags")
+nmap("<leader>fo", function()
     builtin.oldfiles({ only_cwd = true })
-end)
-nmap('<leader>fO', builtin.oldfiles)
-nmap('<leader>fc', function()
+end, "Telescope: oldfiles in cwd")
+nmap("<leader>fO", builtin.oldfiles, "Telescope: oldfiles across all dirs")
+nmap("<leader>fc", function()
     builtin.colorscheme { enable_preview = true, preview = true }
-end)
-nmap('<leader>fd', ':Telescope file_browser<cr>')
-nmap('<leader>fe', ':Telescope gitmoji theme=dropdown<cr>')
-nmap('<leader>ft', ':Telescope treesitter<cr>')
-nmap('<leader>gb', ':Telescope git_branches<cr>')
-nmap('<leader>fs', ':Telescope lsp_document_symbols<cr>')
-nmap('<leader>fa', ':Telescope lsp_dynamic_workspace_symbols<cr>')
-nmap('<leader>fC', ':Telescope commands<cr>')
+end, "Telescope: colorschemes")
+nmap("<leader>fd", ":Telescope file_browser<cr>", "Telescope: file browser")
+nmap("<leader>fe", ":Telescope gitmoji theme=dropdown<cr>", "Telescope: gitmoji")
+nmap("<leader>ft", ":Telescope treesitter<cr>", "Telescope: treesitter document symbols")
+nmap("<leader>gb", ":Telescope git_branches<cr>", "Telescope: git branches")
+nmap("<leader>fs", ":Telescope lsp_document_symbols<cr>", "Telescope: LSP document symbols")
+nmap("<leader>fa", ":Telescope lsp_dynamic_workspace_symbols<cr>", "Telescope: LSP workspace symbols")
+nmap("<leader>fC", ":Telescope commands<cr>", "Telescope: VIM commands")
+nmap("<leader>fm", "<cmd>Telescope marks<cr>", "Telescope: VIM marks")
 
 -- <c-d>/<c-u> - scroll preview
 
 -- Terminal
 
-nmap('<c-`>', ':ToggleTerm<cr>')
-nmap('<c-`>1', ':1ToggleTerm<cr>')
-nmap('<c-`>2', ':2ToggleTerm<cr>')
-nmap('<c-`>3', ':3ToggleTerm<cr>')
-function _G.my_set_terminal_keymaps()
-    local opts = { buffer = 0 }
-    vim.keymap.set('t', '<c-`>', '<cmd>ToggleTerm<cr>', opts)
-    vim.keymap.set('t', '<c-`>1', '<cmd>1ToggleTerm<cr>')
-    vim.keymap.set('t', '<c-`>2', '<cmd>2ToggleTerm<cr>')
-    vim.keymap.set('t', '<c-`>3', '<cmd>3ToggleTerm<cr>')
-    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-end
-
-vim.cmd('autocmd! TermOpen term://* lua my_set_terminal_keymaps()')
+nmap("<c-`>", ":ToggleTerm<cr>", "Terminal: open")
+nmap("<c-`>1", ":1ToggleTerm<cr>", "Terminal: open #1")
+nmap("<c-`>2", ":2ToggleTerm<cr>", "Terminal: open #2")
+nmap("<c-`>3", ":3ToggleTerm<cr>", "Terminal: open #3")
 
 -- Presentation
 
 local font = require("my.font")
-vim.keymap.set({ 'n' }, '<D-=>', font.increase_font)
-vim.keymap.set({ 'n' }, '<D-->', font.decrease_font)
-vim.keymap.set({ 'n' }, '<D-0>', font.reset_font)
-vim.keymap.set({ 'n' }, '<leader>p', font.present)
+nmap('<D-=>', font.increase_font, "Font: increase")
+nmap('<D-->', font.decrease_font, "Font: decrease")
+nmap('<D-0>', font.reset_font, "Font: reset")
+nmap('<leader>p', font.present, "Start presentation")
 
 -- Debugging
 
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
-vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
-vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
-vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end)
-vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
-vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function() require('dap.ui.widgets').hover() end)
-vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function() require('dap.ui.widgets').preview() end)
+nmap('<F5>', function() require('dap').continue() end, "DAP: start listening / continue")
+nmap('<F10>', function() require('dap').step_over() end, "DAP: step over")
+nmap('<F11>', function() require('dap').step_into() end, "DAP: step into")
+nmap('<F12>', function() require('dap').step_out() end, "DAP: step out")
+nmap('<Leader>db', function() require('dap').toggle_breakpoint() end, "DAP: toggle breakpoint")
+nmap('<Leader>dl', function() require('dap').run_last() end, "DAP: run last")
+vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function() require('dap.ui.widgets').hover() end, {
+    desc =
+    "DAP: hover widgets"
+})
+vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function() require('dap.ui.widgets').preview() end, { desc = "DAP: preview" })
 
 nmap("<leader>dR", "<cmd>lua require'dap'.run_to_cursor()<cr>", "Run to Cursor")
 nmap("<leader>dE", "<cmd>lua require'dapui'.eval(vim.fn.input '[Expression] > ')<cr>", "Evaluate Input")
@@ -172,43 +161,44 @@ nmap("<leader>ts", "<cmd>lua require('neotest').summary.toggle()<cr>", "Summary"
 
 -- Misc
 
-nmap('<leader>t', ':NvimTreeFindFile<cr>')
-nmap('<leader>T', ':NvimTreeToggle<cr>')
+nmap("<leader>t", ":NvimTreeFindFile<cr>", "NvimTree: open on current file")
+nmap("<leader>T", ":NvimTreeToggle<cr>", "NvimTree: toggle")
 
-nmap('<leader>so', ':SymbolsOutline<cr>')
+nmap("<leader>so", ":SymbolsOutline<cr>", "Symbols outline")
 
-nmap('<leader>s', ':Alpha<cr>')
+nmap("<leader>s", ":Alpha<cr>", "Open Alpha start page")
 
 vim.keymap.set(
     { "n", "x" },
     "<leader>rr",
-    function() require('refactoring').select_refactor() end
+    function() require("refactoring").select_refactor() end,
+    { desc = "Refactor" }
 )
 
-vim.keymap.set({ "i" }, "<c-t>", '<Plug>luasnip-expand-or-jump')
+vim.keymap.set({ "i" }, "<c-t>", "<Plug>luasnip-expand-or-jump", { desc = "Luasnip: expand or jump" })
 
-nmap('<leader>j',
-    ':%!/opt/homebrew/bin/php -r \'echo json_encode(json_decode(file_get_contents("php://stdin")), JSON_PRETTY_PRINT);\'<cr>:set filetype=json<cr>')
-nmap('<leader>J',
-    ':%!/opt/homebrew/bin/php -r \'echo json_encode(json_decode(file_get_contents("php://stdin")));\'<cr>:set filetype=json<cr>')
+nmap("<leader>j",
+    ':%!/opt/homebrew/bin/php -r \'echo json_encode(json_decode(file_get_contents("php://stdin")), JSON_PRETTY_PRINT);\'<cr>:set filetype=json<cr>',
+    "JSON pretty format")
+nmap("<leader>J",
+    ':%!/opt/homebrew/bin/php -r \'echo json_encode(json_decode(file_get_contents("php://stdin")));\'<cr>:set filetype=json<cr>',
+    "JSON compact format")
 
-nmap("[<cr>", ":Hi {<cr>")
-nmap("]<cr>", ":Hi }<cr>")
+nmap("[<cr>", "<cmd>Hi {<cr>", "Higlight: previous")
+nmap("]<cr>", "<cmd>Hi }<cr>", "Highlight: next")
 
-vim.keymap.set('n', 'g:', function()
+vim.keymap.set("n", "g:", function()
     -- get current buffer and window
     local buf = vim.api.nvim_get_current_buf()
     local win = vim.api.nvim_get_current_win()
     -- create a new split for the repl
-    vim.cmd('split')
+    vim.cmd("split")
     -- spawn repl and set the context to our buffer
-    require('neorepl').new {
-        lang = 'vim',
+    require("neorepl").new {
+        lang = "vim",
         buffer = buf,
         window = win,
     }
     -- resize repl window and make it fixed height
-    vim.cmd('resize 10 | setl winfixheight')
-end)
-
-
+    vim.cmd("resize 10 | setl winfixheight")
+end, { desc = "NeoRepl" })
