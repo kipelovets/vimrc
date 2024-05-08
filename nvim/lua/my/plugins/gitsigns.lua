9e2c94f69068b6a588f1174d9f086320c63a46c7
Georgii Korshunov <georgii.korshunov@chess.com>
2024-05-08 14:44:37 +0200

    🔧 Gitsigns: highlighting line numbers too

--- nvim/lua/my/plugins/gitsigns.lua
+++ nvim/lua/my/plugins/gitsigns.lua
@@ -1,11 +1,11 @@
 return {
     "lewis6991/gitsigns.nvim",
     opts = {
-        signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
-        numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+        signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
+        numhl                        = true, -- Toggle with `:Gitsigns toggle_numhl`
         linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
         attach_to_untracked          = true,
-        current_line_blame           = true,  -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts      = {
             virt_text = true,
             virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
