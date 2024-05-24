ce7838ae418c53bd91cde654a7e7912aac15e21a
Georgii Korshunov <georgii.korshunov@chess.com>
2024-05-24 23:57:05 +0200

    ✨ Added Symfony utils

--- nvim/lua/my/remap.lua
+++ nvim/lua/my/remap.lua
@@ -235,3 +235,5 @@ vim.api.nvim_create_user_command('DiffBranch', function()
     local output = vim.fn.system { 'git', 'merge-base', 'develop', 'HEAD' }
     vim.cmd("Git diff " .. output .. " HEAD")
 end, {})
+
+vim.keymap.set("n", "gsd", "<cmd>GoToSymfonyDefinition<CR>", { desc = "[G]o to [S]ymfony [D]efinition" })
