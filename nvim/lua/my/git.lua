599e589eb611e8be3c468d6c562b15271b868089
Georgii Korshunov <georgii.korshunov@chess.com>
2024-05-27 13:26:29 +0200

    ✨ Making DiffBranch with when base branch is not develop

--- nvim/lua/my/git.lua
+++ nvim/lua/my/git.lua
@@ -1,4 +1,18 @@
 vim.api.nvim_create_user_command('DiffBranch', function()
-    local output = vim.fn.system { 'git', 'merge-base', 'develop', 'HEAD' }
+    local branches = { 'develop', 'master', 'main' }
+    local branch = nil
+    for _, v in ipairs(branches) do
+        local res = vim.fn.system { 'git', 'rev-parse', '--verify', 'refs/heads/' .. v }
+        if nil == string.find(res, "fatal: Needed a single revision") then
+            branch = v
+            break
+        end
+    end
+    if nil == branch then
+        vim.notify("Base branch not found", vim.log.levels.ERROR)
+        return
+    end
+    local output = vim.fn.system { 'git', 'merge-base', branch, 'HEAD' }
+    output = output:gsub("%s+", "")
     vim.cmd("Git diff " .. output .. " HEAD")
 end, {})
