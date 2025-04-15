f04848c1b589ef5f724d289a362a481904642f43
Georgii Korshunov <georgii.korshunov@larian.com>
2025-04-15 12:40:20 +0200

    ✨ PHP: support for interfaces & enums when copying type name

--- nvim/lua/my/php.lua
+++ nvim/lua/my/php.lua
@@ -7,14 +7,19 @@ vim.api.nvim_create_user_command('PhpCopyClassName', function()
     local get_node = function(sexpr)
         local query = vim.treesitter.query.parse('php', sexpr)
         for _, capture in query:iter_captures(root, bufnr, 0, -1) do
-            return vim.treesitter.get_node_text(capture, bufnr)
+            local text = vim.treesitter.get_node_text(capture, bufnr)
+            if text then
+                return text
+            end
         end
         return ""
     end
 
     local ns = get_node '(namespace_definition (namespace_name) @ns)'
     local class = get_node '(class_declaration name:(name) @ns)'
-    local result = ns .. '\\' .. class
+    local interface = get_node '(interface_declaration name:(name) @ns)'
+    local enum = get_node '(enum_declaration name:(name) @ns)'
+    local result = ns .. '\\' .. (class ~= "" and class or (interface ~= "" and interface or enum))
     vim.fn.setreg('"', result)
     print("Classname copied: " .. result)
 end, {})
