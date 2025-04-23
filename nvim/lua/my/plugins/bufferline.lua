c9b9fe67b6322f7118a74d338ea8e2851cd966b4
Georgii Korshunov <georgii.korshunov@larian.com>
2025-04-23 13:47:56 +0200

    🔧 Better behavior for closing buffers && bufferline && nvim-tree

--- nvim/lua/my/plugins/bufferline.lua
+++ nvim/lua/my/plugins/bufferline.lua
@@ -11,7 +11,7 @@ function M.config()
         options = {
             mode = "buffers", -- set to "tabs" to only show tabpages instead
             numbers = "none",
-            close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+            close_command = "SmartQ %d", -- can be a string | function, see "Mouse actions"
             left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
             middle_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
             right_mouse_command = "vertical sbuffer %d", -- can be a string | function, see "Mouse actions"
