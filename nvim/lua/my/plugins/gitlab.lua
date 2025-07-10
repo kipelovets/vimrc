c78dbfd45e503aea22f4615dee1d6b30ceef0c3d
Georgii Korshunov <georgii.korshunov@larian.com>
2025-07-10 12:34:10 +0200

    🔧 Gitlab draft mode by default, UI tweaks

--- nvim/lua/my/plugins/gitlab.lua
+++ nvim/lua/my/plugins/gitlab.lua
@@ -1,14 +1,23 @@
 return {
-  "harrisoncramer/gitlab.nvim",
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
-    "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
-    "nvim-tree/nvim-web-devicons", -- Recommended but not required. Icons in discussion tree.
-  },
-  build = function () require("gitlab.server").build(true) end, -- Builds the Go binary
-  config = function()
-    require("gitlab").setup()
-  end,
+    "harrisoncramer/gitlab.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "stevearc/dressing.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    build = function() require("gitlab.server").build(true) end,
+    config = function()
+        require("gitlab").setup({
+            discussion_tree = {
+                draft_mode = true,
+            },
+            discussion_signs = {
+                severity = vim.diagnostic.severity.WARN,
+                virtual_text = true,
+                priority = 100,
+            },
+        })
+    end,
 }
